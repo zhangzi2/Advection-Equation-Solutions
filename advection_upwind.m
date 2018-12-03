a579182eda782917b827815e17838a10a0fb245c
Kevin Zhang <40212726+zhangzi2@users.noreply.github.com>
2018-12-03 12:46:27 -0800

    Update advection_upwind.m

--- advection_upwind.m
+++ advection_upwind.m
@@ -10,10 +10,17 @@ function [u] = advection_upwind(a,b,dx,dt,max_t,c)
     u = 0*uprev;
     
     for n=1:length(time)  
-        for j=2:length(u)
-            u(j) = uprev(j)-nu*(uprev(j)-uprev(j-1));
+        for j=2:length(u)-1
+            if c > 0
+               u(j) = uprev(j)-nu*(uprev(j)-uprev(j-1));
+               u(end) = uprev(end)-nu*(uprev(end)-uprev(end-1));
+            else
+               u(j) =  uprev(j)-nu*(uprev(j+1)-uprev(j));
+               u(end) = uprev(end)-nu*(uprev(1)-uprev(end));
+            end
             u(1) = u(end); %periodic bc
         end 
         uprev = u;
     end
 end
+
